b3b461dbc2530181e32cbf78d8715fbea1fb17fc
Static Reflection <deng3616888303@outlook.com>
2023-11-10 15:21:30 +0800

    添加项目文件。

---
+++ KernelSU_Patcher.cpp
@@ -0,0 +1,37 @@
+﻿// KernelSU_Patcher.cpp: 定义应用程序的入口点。
+//
+
+#include "KernelSU_Patcher.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	std::vector<std::string> arguments;
+	for (int i = 0; i < argc; i++) {
+		arguments.push_back(argv[i]);
+	}
+	if (argc != 3 && argc != 4) {
+		std::cout << "Usage:" << arguments[0] << " [KernelSU] [Boot] [Output]" << std::endl << std::endl;
+		std::cout << "[KernelSU]\t" << "Path to the AnyKernel3-type KernelSU package." << std::endl;
+		std::cout << "[Boot]\t\t" << "Path to the boot image that needs to be patched." << std::endl;
+		std::cout << "[Output]\t" << "Optional, path to the folder where the output should be saved." << std::endl;
+	}
+	else if ((argc == 3 && isPathValid(arguments[1]) && isPathValid(arguments[2])) || (argc == 4 && isPathValid(arguments[1]) && isPathValid(arguments[2]) && isPathValid(arguments[3]))) {
+		execute(SEVEN_ZIP + " x -y \"" + arguments[1] + "\" Image");
+		execute(MAGISKBOOT + " unpack \"" + arguments[2] + "\"");
+		std::remove("kernel");
+		std::filesystem::rename("Image", "kernel");
+		execute(MAGISKBOOT + " repack \"" + arguments[2] + "\"");
+		execute(MAGISKBOOT + " cleanup");
+		if (argc == 4) {
+			rename("new-boot.img", std::string(arguments[3] + "\\new-boot.img").c_str());
+		}
+	}
+	else {
+		std::cerr << "无效路径" << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
---
+++ KernelSU_Patcher.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <cstdio>
+
+const std::string VERSION = "1.0.0";
+
+const std::string MAGISKBOOT = "bin\\magiskboot\\magiskboot.exe";
+const std::string SEVEN_ZIP = "bin\\7zip\\7z.exe";
+
+//执行程序
+std::string execute(std::string command) {
+    std::string result;
+    FILE* pipe = _popen(command.c_str(), "r");
+    if (!pipe) {
+        return "Failed to execute command.";
+    }
+    char buffer[128];
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        result.append(buffer);
+    }
+    _pclose(pipe);
+    return result;
+}
+//判断路径是否有效
+bool isPathValid(const std::string filePath) {
+    try {
+        std::filesystem::path fsPath(filePath);
+        std::string pathStr = fsPath.generic_string();
+
+        if (std::filesystem::exists(pathStr)) {
+            return true;
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    return false;
+}
